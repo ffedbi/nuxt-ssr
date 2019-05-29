85ecb706a21423479d97fa529fcb00d864a3a11a
fbazan01 <Fedor.Bazanov@isobar.ru>
2019-05-29 19:01:40 +0300

    current progress

---
+++ components/main/comment.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+	el-card.comment
+		.comment__header
+			h6 Person name
+			span
+				i.el-icon-time
+				| {{ new Date().toLocaleString() }}
+		.comment__text Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam et fugit quod. Cupiditate doloremque, ipsam!
+</template>
+
+<script>
+	export default {
+		name: "comment",
+		props: ['comment']
+	}
+</script>
+
+<style scoped lang="scss">
+	.comment {
+		margin-bottom: 1rem;
+
+		&__header {
+			display: flex;
+			justify-content: space-between;
+
+			margin-bottom: 0.5rem;
+
+			font-size: 0.8rem;
+		}
+
+		&__text {
+			white-space: pre-line;
+		}
+	}
+</style>
---
+++ pages/post/_id.vue
@@ -0,0 +1,75 @@
+<template lang="pug">
+	article.post
+		header.post__header
+			.post__title
+				h1 Title
+				nuxt-link(tag="a" to="/")
+					i.el-icon-back
+			.post__info
+				small
+					i.el-icon-time
+						| {{ new Date().toLocaleString() }}
+				small
+					i.el-icon-view
+						| 42
+			.post__img
+				img(:src="`http://picsum.photos/750/400?r=${Math.random()}`" alt="post image").post__img
+		main.post__content
+			p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolorem eius est mollitia officiis pariatur soluta? Ducimus error et hic impedit officiis perspiciatis quia repudiandae!
+			p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolorem eius est mollitia officiis pariatur soluta? Ducimus error et hic impedit officiis perspiciatis quia repudiandae!
+			p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolorem eius est mollitia officiis pariatur soluta? Ducimus error et hic impedit officiis perspiciatis quia repudiandae!
+		footer
+			comment-form
+			.comments(v-if="false")
+				comment(v-for="(comment, index) in 4" :comment="comment" :key="index")
+			.text-center(v-else) Комментариев нет
+</template>
+
+<script>
+	import Comment from "../../components/main/comment";
+	import CommentForm from "../../components/main/comment-form";
+
+	export default {
+		components: {CommentForm, Comment},
+		validate({params}) {
+			console.log(params.id);
+			return !!params.id;
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.post {
+		max-width: 600px;
+		margin: 0 auto;
+
+		&__header {
+			margin-bottom: 1.5rem;
+		}
+
+		&__title {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+
+			margin-bottom: 1rem;
+		}
+
+		&__info {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+
+			margin-bottom: 0.5rem;
+		}
+
+		&__img {
+			width: 100%;
+			height: auto;
+		}
+
+		&__content {
+			margin-bottom: 2rem;
+		}
+	}
+</style>
