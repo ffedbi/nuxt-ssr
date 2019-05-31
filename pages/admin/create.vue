8fbd5c053244239c7f05824c44615c0053d74f41
fbazan01 <Fedor.Bazanov@isobar.ru>
2019-05-31 12:24:19 +0300

    finally admin page

--- pages/admin/create.vue
+++ pages/admin/create.vue
@@ -4,7 +4,16 @@
 		el-form-item(label='Название поста' prop="title")
 			el-input(v-model.trim='controls.title')
 		el-form-item(label='Текст в формате md. или .html' prop="text")
-			el-input(type="textarea" :rows="10" resize="none" v-model.trim='controls.text')
+			el-input(type="textarea" :rows="10" resize="none" v-model='controls.text')
+		el-button(type="success" plain @click="previewDialog = true").mb Предпросмотр
+		el-dialog(title='Предпросмотр' :visible.sync='previewDialog')
+			div(:key="controls.text")
+				vue-markdown {{ controls.text }}
+		el-upload.mb2(drag :auto-upload="false" action='https://jsonplaceholder.typicode.com/posts/' :on-change="handleImageChange" ref="upload")
+			i.el-icon-upload
+			.el-upload__text  Перетащите картинку
+				em &nbsp;или кликните
+			.el-upload__tip(slot='tip') файлы в формате jpg/png
 		el-form-item
 			el-button(type='primary', native-type="submit" round :loading="loading") Создать
 </template>
@@ -17,6 +26,8 @@
 		data() {
 			return {
 				loading: false,
+				previewDialog: false,
+				image: null,
 				controls: {
 					title: '',
 					text: '',
@@ -32,26 +43,34 @@
 			}
 		},
 		methods: {
+			handleImageChange(file, fileList) {
+				console.log(file.raw);
+				this.image = file.raw;
+			},
 			onSubmit() {
 				this.$refs.form.validate(async valid => {
-					if (valid) {
+					if (valid && this.image) {
 						this.loading = true;
 
 						try {
 							const formData = {
 								title: this.controls.title,
 								text: this.controls.text,
+								image: this.image
 							};
 
 							await this.$store.dispatch('post/create', formData);
-							this.$message.success('Пост создан!');
 							this.controls.title = '';
 							this.controls.text = '';
+							this.image = null;
+							this.$refs.upload.clearFiles();
+							this.$message.success('Пост создан!');
 						} catch (e) {
-
 						} finally {
 							this.loading = false;
 						}
+					} else {
+						this.$message.warning('Форма не валидна')
 					}
 				});
 			},
