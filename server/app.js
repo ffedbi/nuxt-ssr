cbdc3bdfcb79c72ca7b70d79be4205ae040a7768
fbazan01 <Fedor.Bazanov@isobar.ru>
2019-06-03 17:18:13 +0300

    fix server error

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const passportStrategy = require('./middleware/passport-strategy');
 const authRoutes = require('./routes/auth.routes');
+const postRoutes = require('./routes/post.routes');
+const commentsRouters = require('./routes/comment.routes');
 const keys = require('./keys');
 const app = express();
 
@@ -18,5 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentsRouters);
 
 module.exports = app;
