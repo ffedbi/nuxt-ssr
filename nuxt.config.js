532cc31544a0e5d05c2613052f5abb3a2d3e3c05
fbazan01 <Fedor.Bazanov@isobar.ru>
2019-06-05 17:20:07 +0300

    fix nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,73 +2,69 @@ const pkg = require('./package')
 
 
 module.exports = {
-  mode: 'universal',
+	mode: 'universal',
 
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+	/*
+	** Headers of the page
+	*/
+	head: {
+		title: pkg.name,
+		meta: [
+			{charset: 'utf-8'},
+			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+			{hid: 'description', name: 'description', content: pkg.description}
+		],
+		link: [
+			{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+		]
+	},
 
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#409eff' },
+	/*
+	** Customize the progress-bar color
+	*/
+	loading: {color: '#409eff'},
 
-  /*
-  ** Global CSS
-  */
-  css: [
-    'element-ui/lib/theme-chalk/index.css',
+	/*
+	** Global CSS
+	*/
+	css: [
+		'element-ui/lib/theme-chalk/index.css',
 		'@theme/style.scss'
-  ],
+	],
 
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-		{ src: '@/plugins/globals', ssr: false },
+	/*
+	** Plugins to load before mounting the App
+	*/
+	plugins: [
+		{src: '@/plugins/globals', mode: 'client'},
 		'@/plugins/axios'
-  ],
+	],
 
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+	/*
+	** Nuxt.js modules
+	*/
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		'@nuxtjs/axios',
 
 		// Doc: https://pwa.nuxtjs.org/setup.html
 		'@nuxtjs/pwa',
-  ],
+	],
 
-	workbox: {
+	workbox: {},
 
-	},
+	meta: {},
 
-	meta: {
-
-	},
-
-  /*
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
+	/*
+	** Axios module configuration
+	*/
+	axios: {
+		// See https://github.com/nuxt-community/axios-module#options
 		baseURL: process.env.BASE_URL || 'http://localhost:3000'
-  },
+	},
 
 	env: {
-  	appName: 'SSR Blog'
+		appName: 'SSR Blog'
 	},
 
 	generate: {
@@ -77,16 +73,16 @@ module.exports = {
 		}
 	},
 
-  /*
-  ** Build configuration
-  */
-  build: {
-    transpile: [/^element-ui/],
+	/*
+	** Build configuration
+	*/
+	build: {
+		transpile: [/^element-ui/],
 
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
-  }
+		/*
+		** You can extend webpack config here
+		*/
+		extend(config, ctx) {
+		}
+	}
 }
